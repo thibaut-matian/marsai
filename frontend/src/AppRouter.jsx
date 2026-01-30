4d2a413f553eab5bb1b934d30d236dfe5da79742
wendy-asmatico <wendy.asmatico@laplateforme.io>
2026-01-30 14:28:29 +0100

    creation navbar

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Header from "./components/layout/Header.jsx";
 
 function AppRouter() {
   return (
     <Router>
+          <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
