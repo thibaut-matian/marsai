ffa01a831249be10a42dd139b76208de7596e4df
geoffrey-badie <geoffrey.badie@laplateforme.io>
2026-01-30 13:56:13 +0100

    loginform statique terminé

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import LoginForm from "./components/features/auth/LoginForm.jsx";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </Router>
   );
