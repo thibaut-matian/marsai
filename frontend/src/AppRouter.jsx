ba4b01a18547906352c0ec46119bc278800d8d7a
wendy-asmatico <wendy.asmatico@laplateforme.io>
2026-02-02 16:31:28 +0100

    footer fini

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Header from "./components/layout/Header.jsx";
+import MainLayout from "./components/layout/MainLayout.jsx";
 import FAQ from "./pages/FAQ.jsx";
 import Planning from "./pages/Planning.jsx";
 {/*import Submission from "./pages/Submission.jsx";*/}
@@ -8,12 +8,13 @@ import Planning from "./pages/Planning.jsx";
 function AppRouter() {
   return (
     <Router>
-          <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/FAQ" element={<FAQ />} />
-        <Route path="/planning" element={<Planning />} /> 
-        {/*<Route path="/submission" element={<Submission />} />*/}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="/planning" element={<Planning />} /> 
+          {/*<Route path="/submission" element={<Submission />} />*/}
+        </Route>
       </Routes>
     </Router>
   );
