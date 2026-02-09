e15fc4eee4f87a537532ef9796d5860e9e1f933b
Theubald-bee <thibaut.matian@gmail.com>
2026-02-09 13:31:19 +0100

    correction du main layout

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer.jsx";
 
 export default function MainLayout() {
   return (
-    <div className="flex flex-col min-h-screen bg-black overflow-hidden">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
         <Outlet />
