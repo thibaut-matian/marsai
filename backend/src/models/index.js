5d5c49cc67eb17dcae3d2893e43aee7efc2d7309
veronique-marro <veronique.marro@laplateforme.io>
2026-02-13 15:25:04 +0100

    Creation social media model

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,36 +3,57 @@ const SocialLink = require("./SocialLinksModel");
 const Squad = require("./SquadModel");
 const Booking = require("./BookingModel");
 const MoviesScreenshots = require("./MoviesScreenshotsModel");
+const SocialMedia = require("./SocialMediasModel");
+const Event = require("./EventsModel"); // Ajouté pour la cohérence globale
 
 // --- DÉFINITION DES RELATIONS ---
 
-// Un film peut avoir plusieurs collaborateurs (Scénariste, Monteur, etc.)
+// 1. RELATION SQUAD (ÉQUIPE)
+// Un film possède plusieurs collaborateurs
 Movie.hasMany(Squad, {
   foreignKey: "movie_id",
-  as: "team", // Alias utilisé pour la soumission groupée du formulaire [cite: 38, 42]
+  as: "team",
 });
-
-// Chaque collaborateur est lié à un seul film
 Squad.belongsTo(Movie, {
   foreignKey: "movie_id",
 });
 
+// 2. RELATION RÉSEAUX SOCIAUX
 Movie.belongsToMany(SocialLink, {
-  through: "movies_socials", // Table de jointure définie dans votre SQL
-  foreign_key: "movie_id",
+  through: "movies_socials",
+  foreignKey: "movie_id",
   otherKey: "social_id",
 });
-
-// Chaque collaborateur est lié à un seul film
-MoviesScreenshots.belongsTo(Movie, {
-  foreignKey: "movie_id",
+// Important : Un lien (URL) appartient à une plateforme spécifique (ex: Instagram).
+SocialLink.belongsTo(SocialMedia, {
+  foreignKey: "social_id",
+  as: "platform",
 });
 
-// Un film peut avoir plusieurs screenshots
+// 3. RELATION SCREENSHOTS (CAPTURES D'ÉCRAN)
 Movie.hasMany(MoviesScreenshots, {
   foreignKey: "movie_id",
   as: "screenshots",
 });
+MoviesScreenshots.belongsTo(Movie, {
+  foreignKey: "movie_id",
+});
 
-// Export des modèles pour utilisation dans server.js
-module.exports = { Movie, Squad, MoviesScreenshots, SocialLink, Booking };
+// 4. RELATION BILLETTERIE (EVENTS & BOOKINGS)
+Event.hasMany(Booking, {
+  foreignKey: "event_id",
+});
+Booking.belongsTo(Event, {
+  foreignKey: "event_id",
+});
+
+// Ajout de SocialMedia et Event dans l'export pour server.js.
+module.exports = {
+  Movie,
+  Squad,
+  MoviesScreenshots,
+  SocialLink,
+  SocialMedia,
+  Booking,
+  Event,
+};
