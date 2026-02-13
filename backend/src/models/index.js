b164ca5a016946471446a2f1ac386fa63b773816
wendy-asmatico <wendy.asmatico@laplateforme.io>
2026-02-13 15:46:42 +0100

    changement index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,8 +1,8 @@
 const Movie = require("./MoviesModel");
 const Squad = require("./SquadModel");
 const MoviesScreenshots = require("./MoviesScreenshotsModel");
+const Note = require("./NoteModel");
 
-// --- DÉFINITION DES RELATIONS ---
 
 // Un film peut avoir plusieurs collaborateurs (Scénariste, Monteur, etc.)
 Movie.hasMany(Squad, {
@@ -26,5 +26,20 @@ Movie.hasMany(MoviesScreenshots, {
   as: "screenshots",
 });
 
+Movie.hasOne(Note, { 
+    foreignKey: "movie_id", 
+    onDelete: "CASCADE" 
+});
+
+Note.belongsTo(Movie, { 
+    foreignKey: "movie_id" 
+});
+
+// La Note est créée par un Utilisateur (Modérateur)
+// Un modérateur peut écrire plusieurs notes (sur des films différents), 
+// mais la Note, elle, n'a qu'un seul auteur.
+User.hasMany(Note, { foreignKey: "user_id" });
+Note.belongsTo(User, { foreignKey: "user_id", as: "Jury" });
+
 // Export des modèles pour utilisation dans server.js
-module.exports = { Movie, Squad, MoviesScreenshots };
+module.exports = { Note, Movie, Squad, MoviesScreenshots };
